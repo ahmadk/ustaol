d89c0f9e074c92963981b7bf67696b45b127cb07
Wael <wnkhalifa99@gmail.com>
2020-02-09 00:44:49 +0300

    added bank icons and links to items in footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faMapMarkerAlt, faPhoneAlt, faAt } from '@fortawesome/free-solid-svg-icons';
-import { faFacebookF, faGoogle, faTwitter, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF, faGoogle, faTwitter, faLinkedin, faInstagram, faCcVisa, faCcMastercard } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -16,6 +16,8 @@ export class FooterComponent implements OnInit {
   faTwitter = faTwitter;
   faLinkedin = faLinkedin;
   faInstagram = faInstagram;
+  faCcVisa = faCcVisa;
+  faCcMastercard = faCcMastercard;
 
   constructor() {
   }
